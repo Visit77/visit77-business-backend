d7887f0933ce18ef4608b0074d773c0206ddaee0
waiyanmoeoo <waiyanmoeoo81@gmail.com>
2026-09-15 14:05:56 +0700

    fix bug

--- booking/views.py
+++ booking/views.py
@@ -1191,11 +1191,14 @@ class PublicDemoPaymentView(APIView):
         # Send confirmation email only after successful DB commit
         if booking.status == Booking.Status.CONFIRMED:
             booking_id = str(booking.id)
-            def send_booking_notifications():
-                send_booking_confirmation_email_task.delay(booking_id)
-                send_booking_confirmation_sms_task.delay(booking_id)
+            send_booking_confirmation_email_task.delay(booking_id)
+            send_booking_confirmation_sms_task.delay(booking_id)
 
-            transaction.on_commit(send_booking_notifications)
+            # def send_booking_notifications():
+            #     send_booking_confirmation_email_task.delay(booking_id)
+            #     send_booking_confirmation_sms_task.delay(booking_id)
+
+            # transaction.on_commit(send_booking_notifications)
             # transaction.on_commit(
             #     lambda: send_booking_confirmation_email_task(
             #         booking_id
@@ -1337,12 +1340,14 @@ class CorePaymentSuccessView(APIView):
         #
         if booking.status == Booking.Status.CONFIRMED:
             booking_id = str(booking.id)
+            send_booking_confirmation_email_task.delay(booking_id)
+            send_booking_confirmation_sms_task.delay(booking_id)
 
-            def send_booking_notifications():
-                send_booking_confirmation_email_task.delay(booking_id)
-                send_booking_confirmation_sms_task.delay(booking_id)
+            # def send_booking_notifications():
+            #     send_booking_confirmation_email_task.delay(booking_id)
+            #     send_booking_confirmation_sms_task.delay(booking_id)
 
-            transaction.on_commit(send_booking_notifications)
+            # transaction.on_commit(send_booking_notifications)
 
             # transaction.on_commit(
             #     lambda: send_booking_confirmation_email_task.delay(
